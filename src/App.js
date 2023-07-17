8197a84b47db89291d80ae0ea4bf396841f6e562
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-07-17 14:39:14 -0300

    Projeto organizado!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <h1>Alura Space</h1>
   );
 }
-
-export default App;
